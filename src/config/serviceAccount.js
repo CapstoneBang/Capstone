04f1575c24b8a5f6d31d720ec27988faa626901e
CapstoneBang <c293bsy4025@bangkit.academy>
2023-12-14 00:49:28 +0700

    add login and register API

---
+++ src/config/serviceAccount.js
@@ -0,0 +1,16 @@
+export default {
+  type: "service_account",
+  project_id: "hydrogrow-d1a05",
+  private_key_id: "d7c56b3e90016c47e4e078f965cb1ca9dfb84cb7",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQD0uVrPK1wNn0ec\nKQ+bB+T3mGDjm9WDIgyuuvLG8twjF0vMqG9C/5AABl28h+cSKmcb9oAShW7OyD8X\nC5/tEXs6gfm01GDTbSB521l8Oja8/PSMWPLxDPerXih4kc49W93pBIclIRv8Qre6\nob+QHuiZPc+RL+HbvPfydOieyVx0EAV3fuQyM5GgSdoRWT5P1y/0TxEysQ0qCTlR\n67fYoySyfT7HXwdvommfc5YWMCFuUlsM+clttLtql13YYFEMuRvoizyHnuKBVffI\nqdjMzWsOFnOjhWv+YWAABQJrL+015MUn+QHW2WGmrjilMD3b9n8JkCJI8n7oAXBp\nB4V14iOnAgMBAAECggEAJaLn+1qAwruPSM3rz3zsUsPFitupf4xaBPlNYxlwj5kC\nmEwDKHujSs2v1tHT0LBiwSMs2SxaOUUEpuerxcrdd8cLfyr2ZS2wQ+1fGsLdLj18\n4qS/YCENafaGJNzPqhIZ079VhXlNE4OzO+mqS2DlhsPBy2NgyemK/GfMarcvF84d\n92GYyVxS1guQWTgph7luDjSYbtCYSPdFOD3gvzCBmDJtJGzMJeCsJ6k3BqQ+hnqH\nfpMG9qYP8l92G1BKR3DylkcQBjyUmkvVC8Y4FLbZ5ZzCsj/xrxIRYNmI82iYpoRi\nQdrxEq/al27I0g5SOA5DDoaerq6Qo0wSLlZ1GcnXWQKBgQD/0D533VDBMDB9lSBz\nqufWPyXxa4nOfaNOSjSBoRIVlg0uenwz98LXtNFL8HIMtMMANOIQtjdZuCq625EG\nB7C44Cui40JDSyREyoNxusbQliouljLPjM4IWW5pgpC4afxEoCLaaZif1YAKGEQS\n2GUjIpyPJTvCfi1ZKeilHv48WwKBgQD05wpefXIpwg124XNea6kTdltfi5dUYCkb\neqUttOFtlCtzkJOqyWjnRnf09jAlKk5oz6WWsJsb8HbvxoVxLSHwGE7HM2vDljFP\nO/CcSKLYOlaVY9EM/N8y5mTE01lHxFadLMzcE4tZEBxE1GsH4IenUXWJtWxvTfKI\nUGa+zlhHpQKBgQD2r/u9nVfFpNHuGMP9pv8A1U6ivABddzznP5nYs9n7pQK7InTp\nrHBhkUYQBCQHar1tUpH6aa2AwJ+8KAZ3GS1rawnJf3wj5q9oAx60qtgUx+qPmFoR\nWghJduyJpNYocFRMXVye4RvEbp+VnIbw4WrOcg8l31L8oK7cgBSQPw4OZwKBgHRk\nVdZAkoBsHJASaA1I54s7ZgPLyPkt1nub12MYo0a788axwKybmoLIR/yhAJK5P5iq\nj01F1AH8T9p3JDZ8t7NvKgmcU6wZ9odsulbcYqCXYU0Qqmscx0temie9NqJYGr4K\na7Zwh3qloHnJ5rBQssf/e/gdxFJF3m2QoLQwR16NAoGBALmL+YKExTT4bEneJbiz\nCZFwts3vyYhq3frIdyotTKp6bDxvJ+djDQyvtWYhwozQHXJLxzVPA98PciC9+PUh\nJOb20NyHQ0VTF1tlMcPRHBZQAIGB9q1Gt0trx3RNWy8yK7mlFJO0AdCepbQnSBXi\npSNkpPGFsOd6IX4zEBrHVWZC\n-----END PRIVATE KEY-----\n",
+  client_email:
+    "firebase-adminsdk-cdbpf@hydrogrow-d1a05.iam.gserviceaccount.com",
+  client_id: "107534704304590966332",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-cdbpf%40hydrogrow-d1a05.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com",
+};
